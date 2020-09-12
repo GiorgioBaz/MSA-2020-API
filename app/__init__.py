abf74e42c213bdc5335d4dc7aecf6c9e0fd63563
giorgiobaz <giorgio.bazouni@gmail.com>
2020-09-12 15:40:42 +1000

    Added Azure SQL Connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,29 @@
 import os
+import urllib.parse
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 
-# Get the directory path of our file
-basedir = os.path.abspath(os.path.dirname(__file__))
+db_string = "Driver={ODBC Driver 13 for SQL Server};Server=tcp:flask-todo.database.windows.net,1433;Database=flask_todo;Uid=flaskadmin;Pwd={Abc-1234};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+params = urllib.parse.quote_plus(db_string)
+conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
+engine_azure = create_engine(conn_str, echo=True)
 
-# Create a path to our sqlite file based on the directory path above
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+#Initialisation
+app.config['SECRET_KEY'] = 'supersecret'
+# app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Get the directory path of our file
+# basedir = os.path.abspath(os.path.dirname(__file__))
+
+# # Create a path to our sqlite file based on the directory path above
+# app.config['SQLALCHEMY_DATABASE_URI'] =\
+#     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Wrap our Flask-Alchemy instance around our Flask Application
 db = SQLAlchemy(app)
