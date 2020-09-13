a529551f69caeffd60564e28366441092105102e
giorgiobaz <giorgio.bazouni@gmail.com>
2020-09-14 02:50:38 +1000

    Devloped basic API calls to Fortnite and Apex API's and stats page

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,24 @@
-import os
-import urllib.parse
+# import os
+# import urllib.parse
+
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine
-
+# from app import routes
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import create_engine
 app = Flask(__name__)
+from app import routes
+# db_string = "Driver={ODBC Driver 13 for SQL Server};Server=tcp:flask-todo.database.windows.net,1433;Database=flask_todo;Uid=flaskadmin;Pwd={Abc-1234};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+# params = urllib.parse.quote_plus(db_string)
+# conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
+# engine_azure = create_engine(conn_str, echo=True)
 
-db_string = "Driver={ODBC Driver 13 for SQL Server};Server=tcp:flask-todo.database.windows.net,1433;Database=flask_todo;Uid=flaskadmin;Pwd={Abc-1234};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
-params = urllib.parse.quote_plus(db_string)
-conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
-engine_azure = create_engine(conn_str, echo=True)
-
-#Initialisation
-app.config['SECRET_KEY'] = 'supersecret'
+# # Initialisation
+# app.config['SECRET_KEY'] = 'supersecret'
 # app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Get the directory path of our file
+# # Get the directory path of our file
 # basedir = os.path.abspath(os.path.dirname(__file__))
 
 # # Create a path to our sqlite file based on the directory path above
@@ -25,23 +26,23 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Wrap our Flask-Alchemy instance around our Flask Application
-db = SQLAlchemy(app)
+# # Wrap our Flask-Alchemy instance around our Flask Application
+# db = SQLAlchemy(app)
 
-from app import routes, db
 
-from app.models import Task
 
-db.drop_all()
-db.create_all()
+# from app.models import Task
 
-tasks = [
-    'Eat some food',
-    'Listen to some music',
-    'Read a book',
-]
+# db.drop_all()
+# db.create_all()
 
-for task in tasks:
-    new_task = Task(name=task, description='')
-    db.session.add(new_task)
-db.session.commit()
+# tasks = [
+#     'Eat some food',
+#     'Listen to some music',
+#     'Read a book',
+# ]
+
+# for task in tasks:
+#     new_task = Task(name=task, description='')
+#     db.session.add(new_task)
+# db.session.commit()
