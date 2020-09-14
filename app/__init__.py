c1337b14a2a50596f9fa8914896ad505e9a8a4cd
giorgiobaz <giorgio.bazouni@gmail.com>
2020-09-14 16:14:12 +1000

    Finalised the entire game stats page, including API calls and bootstrap styling

--- app/__init__.py
+++ app/__init__.py
@@ -1,48 +1,4 @@
-# import os
-# import urllib.parse
-
 from flask import Flask
-# from app import routes
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy import create_engine
+
 app = Flask(__name__)
 from app import routes
-# db_string = "Driver={ODBC Driver 13 for SQL Server};Server=tcp:flask-todo.database.windows.net,1433;Database=flask_todo;Uid=flaskadmin;Pwd={Abc-1234};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
-# params = urllib.parse.quote_plus(db_string)
-# conn_str = 'mssql+pyodbc:///?odbc_connect={}'.format(params)
-# engine_azure = create_engine(conn_str, echo=True)
-
-# # Initialisation
-# app.config['SECRET_KEY'] = 'supersecret'
-# app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
-# app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# # Get the directory path of our file
-# basedir = os.path.abspath(os.path.dirname(__file__))
-
-# # Create a path to our sqlite file based on the directory path above
-# app.config['SQLALCHEMY_DATABASE_URI'] =\
-#     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# # Wrap our Flask-Alchemy instance around our Flask Application
-# db = SQLAlchemy(app)
-
-
-
-# from app.models import Task
-
-# db.drop_all()
-# db.create_all()
-
-# tasks = [
-#     'Eat some food',
-#     'Listen to some music',
-#     'Read a book',
-# ]
-
-# for task in tasks:
-#     new_task = Task(name=task, description='')
-#     db.session.add(new_task)
-# db.session.commit()
